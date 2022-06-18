22d3b72d82012c5c4a77add7d52c7e7500aefb0a
Dan Dumitriu <dandumitriu33@gmail.com>
2022-06-18 06:29:29 +0300

    Add initial lines

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dandumitriu33/useful-go
+
+go 1.18
---
+++ useful.go
@@ -0,0 +1,12 @@
+// main package which runs other components.
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Hello world!")
+	log.Println("Hello world!")
+	log.Println("Hello world!")
+}
